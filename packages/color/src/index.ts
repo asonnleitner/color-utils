46fc9e6d917c3c333ecabb7d3e7f7402f22d3a59
asonnleitner <andreassonnleitner@gmail.com>
2022-04-28 23:19:44 +0200

    feat: rgb, hsl, hwb, hex + tests

---
+++ packages/color/src/index.ts
@@ -0,0 +1,35 @@
+import { HWB, getHWB } from './hwb'
+import { isString } from './utils'
+import { getHSL } from './hsl'
+import { getRGB } from './rgb'
+import type { NamedColor } from '@color-utils/colors'
+import type { Transparent } from './types'
+import type { RGB } from './rgb'
+import type { HSL } from './hsl'
+
+export * from './rgb'
+export * from './hsl'
+export * from './hwb'
+export * from './named'
+export * from './hex'
+
+export function getColor<C extends string | NamedColor | Transparent>(
+  color?: C
+): RGB<number, number, number, number> | undefined
+export function getColor<C extends string>(
+  color?: C
+): HSL<number, number, number, number> | undefined
+export function getColor<C extends string>(
+  color?: C
+): HSL<number, number, number, number> | undefined
+export function getColor(color?: any): any {
+  if (!color || !isString(color)) return undefined
+  color = String(color).toLowerCase()
+
+  const isHSL = color.startsWith('hsl')
+  const isHWB = color.startsWith('hwb')
+
+  if (isHSL) return getHSL(color)
+  if (isHWB) return getHWB(color)
+  return getRGB(color)
+}
